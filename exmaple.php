4a84dd02a76f3fc5c84bcec66b0e3b5396ed8725
mjhead <andremjhead@gmail.com>
2015-10-27 11:11:35 +0200

    Initial Commit

---
+++ exmaple.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Example: add options in cerryframework4 child theme
+ */
+add_action( 'after_setup_theme', 'themeXXXX_custom_options' );
+function themeXXXX_custom_options() {
+
+	require_once CHILD_DIR . '/inc/class-cherry-child-custom-options.php';
+
+	$options = array(
+		'child-menu-bg' => array(
+			'section'          => 'navigation-section',
+			'is-var'           => true,
+			'type'             => 'background',
+			'title'            => __('Navigation panel background', 'themeXXXX' ),
+			'description'      => __( 'Set background for navigation panel. ', 'themeXXXX' ),
+			'hint'             => array(
+				'type'    => 'text',
+				'content' => __('Specify background for header navigation panel', 'themeXXXX' ),
+			),
+			'return_data_type' => 'url',
+			'library_type'     => 'image',
+			'value'            => array(
+				'image'      => '',
+				'color'      => '#333333',
+				'repeat'     => 'repeat',
+				'position'   => 'left',
+				'attachment' => 'fixed',
+				'clip'       => 'padding-box',
+				'size'       => 'cover',
+				'origin'     => 'padding-box',
+			)
+		),
+		'typography-header-sub-menu' => array(
+			'section'          => 'navigation-section',
+			'is-var'           => true,
+			'type'        => 'typography',
+			'title'       => __( 'Header Sub Menu Typography', 'themeXXXX' ),
+			'description' => __( 'Header navigation sub menu typography settings.', 'themeXXXX' ),
+			'value'       => array(
+				'fonttype'      => 'web',
+				'size'          => '16',
+				'lineheight'    => '20',
+				'color'         => '#ffffff',
+				'family'        => 'Roboto Slab',
+				'character'     => 'latin-ext',
+				'style'         => '300',
+				'letterspacing' => '',
+				'align'         => 'notdefined',
+			)
+		),
+		'typography-slogan-child' => array(
+			'section'          => 'logo-subsection',
+			'is-var'           => true,
+			'type'        => 'typography',
+			'title'       => __( 'Header Slogan Typography', 'themeXXXX' ),
+			'description' => __( 'Header slogan typography settings.', 'themeXXXX' ),
+			'value'       => array(
+				'fonttype'      => 'web',
+				'size'          => '22',
+				'lineheight'    => '30',
+				'color'         => '#ffffff',
+				'family'        => 'Roboto Slab',
+				'character'     => 'latin-ext',
+				'style'         => '300',
+				'letterspacing' => '',
+				'align'         => 'notdefined',
+			)
+		),
+	);
+
+	new Cherry_Child_Custom_Options( $options );
+
+}
